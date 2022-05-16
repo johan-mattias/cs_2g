438b83a21b58216d4e40f53fa9961b231f78e8cb
Johan <johan.snider@gmail.com>
2022-05-16 13:58:50 +0200

    messing around

--- cs_test.py
+++ cs_test.py
@@ -4,7 +4,9 @@ Rect(50,50,150,150, fill='blue')
 
 bob = Image('bob.png', 20, 20)
 overture = Sound('1812.mp3')
+
 print(overture)
+
 overture.play()
 bob.width=60
 bob.height=60
---
+++ temp.py
@@ -0,0 +1,5 @@
+from cmu_graphics import *
+
+Rect(50,50,150,150, fill='blue')
+
+cmu_graphics.run()
